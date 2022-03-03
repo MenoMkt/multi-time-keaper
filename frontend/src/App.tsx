b066d20da4471ede97d55ac7fa7dd7649950bcbd
meno.m <meno.m@opst.co.jp>
2022-03-03 15:24:06 +0900

    [add]ヘッダー追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./App.scss";
 import { Container } from "@mui/material";
 import TimerCard from "./component/Card";
-
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import AddIcon from "@mui/icons-material/Add";
+import Tooltip from "@mui/material/Tooltip";
 function App() {
   useEffect(() => {
     // Notification API 初期化
@@ -15,9 +21,27 @@ function App() {
       Notification.requestPermission().then(console.log);
     }
   }, []);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <AppBar position="static" sx={{ mb: 1 }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            MultiTimeKeeper
+          </Typography>
+          <Tooltip title="タイマーを追加する" placement="bottom">
+            <IconButton
+              size="medium"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <AddIcon fontSize="large"></AddIcon>
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
       <main>
         <Container fixed>
           <TimerCard></TimerCard>
