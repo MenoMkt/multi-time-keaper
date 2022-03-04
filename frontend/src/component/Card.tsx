62e9eec705f4aa0d0426f956c7126374eb4990a9
meno.m <meno.m@opst.co.jp>
2022-03-04 17:36:25 +0900

    [add]プログレスバーを表示する

--- frontend/src/component/Card.tsx
+++ frontend/src/component/Card.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  ProfilerOnRenderCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import PropTypes from "prop-types";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
@@ -10,6 +16,7 @@ import {
   IconButton,
   TextField,
   Typography,
+  LinearProgress,
 } from "@mui/material";
 
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
@@ -26,6 +33,11 @@ import Countdown, {
 type Props = {
   title: string;
 };
+type Progress = {
+  value: number;
+  startDate: number;
+  endDate: number;
+};
 
 const TimerCard = (props: Props) => {
   const [time, setTime] = useState(new Date());
@@ -34,6 +46,11 @@ const TimerCard = (props: Props) => {
   const [titleEditMode, setTitleEditMode] = useState(false);
   const [countdownDate, setCountdownDate] = useState(Date.now());
   const [running, setRunning] = useState(false);
+  const [progress, setProgress] = useState<Progress>({
+    value: 0,
+    startDate: Date.now(),
+    endDate: Date.now(),
+  });
 
   let countdownApi: CountdownApi | undefined = undefined;
   // Renderer callback with condition
@@ -56,6 +73,11 @@ const TimerCard = (props: Props) => {
       .set("minute", time.getMinutes())
       .set("second", time.getSeconds());
     setCountdownDate(timeDate.valueOf());
+    setProgress({
+      value: 0,
+      startDate: Date.now(),
+      endDate: time.valueOf(),
+    });
     countdownApi?.start();
     setRunning(true);
   };
@@ -69,8 +91,18 @@ const TimerCard = (props: Props) => {
     console.log("on complete timer");
     timerAlert();
     setRunning(false);
+    countdownApi?.stop();
+    // TODO:タイマー終了時に残り時間を00:00:00にしたいが、カウントダウンの日時を設定すると再度アラートが発火する
+    // setCountdownDate(Date.now());
   };
-
+  const onCountdownTick = () => {
+    setProgress((state) => ({
+      ...state,
+      value:
+        ((Date.now() - state.startDate) * 100) /
+        (state.endDate - state.startDate),
+    }));
+  };
   /**
    * Notification APIで通知する
    */
@@ -113,112 +145,124 @@ const TimerCard = (props: Props) => {
           width: "500px",
         }}
       >
-        <CardContent
-          sx={{
-            alignItems: "flex-start",
-            display: "flex",
-          }}
-        >
+        <CardContent>
           <Box
             sx={{
+              alignItems: "flex-start",
               display: "flex",
-              flexDirection: "column",
             }}
           >
             <Box
               sx={{
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
               }}
             >
-              <TextField
-                id="title "
-                label="title"
-                defaultValue={title}
-                inputRef={tmpTitle}
-                onChange={(v) => {
-                  tmpTitle.current = v.target.value;
-                }}
+              <Box
                 sx={{
-                  width: "200px",
-                }}
-                style={!titleEditMode ? { display: "none" } : {}}
-              />
-              <Typography
-                component="div"
-                variant="h5"
-                textAlign={"left"}
-                sx={{
-                  pl: 1,
-                  pt: 2,
-                  pb: 1,
-                  width: "200px",
-                  boxSizing: "border-box",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                }}
-                style={titleEditMode ? { display: "none" } : {}}
-              >
-                {title}
-              </Typography>
-              <IconButton
-                aria-label="edit-title"
-                onClick={onChangeTitleEditMode}
-                sx={{
-                  mt: 1,
-                  mb: 1,
+                  display: "flex",
+                  flexDirection: "row",
                 }}
               >
-                {titleEditMode ? <CheckCircleIcon /> : <EditIcon />}
-              </IconButton>
+                <TextField
+                  id="title "
+                  label="title"
+                  defaultValue={title}
+                  inputRef={tmpTitle}
+                  onChange={(v) => {
+                    tmpTitle.current = v.target.value;
+                  }}
+                  sx={{
+                    width: "200px",
+                  }}
+                  style={!titleEditMode ? { display: "none" } : {}}
+                />
+                <Typography
+                  component="div"
+                  variant="h5"
+                  textAlign={"left"}
+                  sx={{
+                    pl: 1,
+                    pt: 2,
+                    pb: 1,
+                    width: "200px",
+                    boxSizing: "border-box",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                  }}
+                  style={titleEditMode ? { display: "none" } : {}}
+                >
+                  {title}
+                </Typography>
+                <IconButton
+                  aria-label="edit-title"
+                  onClick={onChangeTitleEditMode}
+                  sx={{
+                    mt: 1,
+                    mb: 1,
+                  }}
+                >
+                  {titleEditMode ? <CheckCircleIcon /> : <EditIcon />}
+                </IconButton>
+              </Box>
+              {/* タイマーコントロール */}
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <IconButton
+                  aria-label="play/pause"
+                  onClick={onChangeStartPauseButton}
+                >
+                  {running ? <PauseIcon /> : <PlayArrowIcon />}
+                </IconButton>
+                <IconButton aria-label="reset">
+                  <ReplayIcon />
+                </IconButton>
+              </Box>
             </Box>
-            {/* タイマーコントロール */}
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <IconButton
-                aria-label="play/pause"
-                onClick={onChangeStartPauseButton}
-              >
-                {running ? <PauseIcon /> : <PlayArrowIcon />}
-              </IconButton>
-              <IconButton aria-label="reset">
-                <ReplayIcon />
-              </IconButton>
+            {/* 右側の要素 */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "baseline",
+                flexDirection: "column",
+              }}
+            >
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <TimePicker
+                  label="Basic example"
+                  ampm={false}
+                  value={time}
+                  onChange={(newValue) => {
+                    if (newValue) {
+                      setTime(newValue);
+                    }
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+              <Box sx={{ width: 1 }}>
+                <Countdown
+                  date={countdownDate}
+                  renderer={countdownRender}
+                  ref={(ref) => {
+                    countdownApi = ref?.getApi();
+                  }}
+                  onComplete={completeTimer}
+                  onTick={onCountdownTick}
+                  overtime={true}
+                  autoStart={false}
+                  zeroPadTime={3}
+                />
+              </Box>
             </Box>
           </Box>
-          {/* 右側の要素 */}
           <Box
             sx={{
-              display: "flex",
-              alignItems: "baseline",
-              flexDirection: "column",
+              mt: 1,
+              ml: 1,
+              mr: 1,
             }}
           >
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <TimePicker
-                label="Basic example"
-                ampm={false}
-                value={time}
-                onChange={(newValue) => {
-                  if (newValue) {
-                    setTime(newValue);
-                  }
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-            <Box sx={{ width: 1 }}>
-              <Countdown
-                date={countdownDate}
-                renderer={countdownRender}
-                ref={(ref) => {
-                  countdownApi = ref?.getApi();
-                }}
-                onComplete={completeTimer}
-                overtime={true}
-                autoStart={false}
-                zeroPadTime={3}
-              />
-            </Box>
+            <LinearProgress variant="determinate" value={progress.value} />
           </Box>
         </CardContent>
       </Card>
